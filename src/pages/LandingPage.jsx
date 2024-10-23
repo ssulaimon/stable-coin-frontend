8abe46e61dc9fd6af31b6ff4a005d092d6edfb92
sssulaimon <ssalaudeen10@gmail.com>
2024-10-23 22:23:22 +0100

    frontend done

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -11,19 +11,36 @@ import { NewsLetter } from "../components/landingPage/NewsLetter"
 import { Footer } from "../components/landingPage/Footer"
 export function LandingPage(){
     return <div>
+    
     <Header/>
-    <Banner/>
+   
+   <div className="container-one">
+   <Banner/>
+   </div>
+ 
     <Divider/>
+    <div className="container-one">
     <LiquidityAccess/>
+    </div>
+   
     <Divider/>
+    <div className="container-one">
     <HowItWorks/>
+    </div>
+   
     <Divider/>
+    <div className="container-one">
     <Discovery/>
+    </div>
+   
     <Divider/>
+    <div className="container-one">
     <Ecosystem/>
 <Faq/> 
 <Blog/>   
 <NewsLetter/>
 <Footer/>
+    </div>
+ 
     </div>
 }
