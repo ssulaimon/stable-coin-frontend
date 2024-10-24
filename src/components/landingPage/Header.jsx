5a41e5ac8b1089a1edc67358a42ec558eb54ee65
sssulaimon <ssalaudeen10@gmail.com>
2024-10-24 08:57:21 +0100

    completed landing page

--- src/components/landingPage/Header.jsx
+++ src/components/landingPage/Header.jsx
@@ -1,5 +1,9 @@
 import "../../styles/landingPage/Header.css";
+import { useState } from "react";
+
 export function Header(){
+    const [showNav, updateShowNav] = useState(false);
+   
     return <header>
         <h1>
             <a href="" id="home-link">USDNDAO</a>
@@ -8,12 +12,12 @@ export function Header(){
             <button className="header-btn" id="launch-app-header">
                 LAUNCH APP
             </button>
-            <button className="header-btn" id="menu-btn">
+            <button className="header-btn" id="menu-btn" onClick={()=> updateShowNav((currentState)=> !currentState)}>
                 MENU
             </button>
             
         </div>
-        <nav>
+        <nav className={showNav ? 'show-nav': 'hide-nav'} id="nav" >
                 <ul>
                     <li>
                         <a href="" className="nav-links">DOCS</a>
--- src/components/landingPage/HowItWorks.jsx
+++ src/components/landingPage/HowItWorks.jsx
@@ -2,13 +2,33 @@ import "../../styles/landingPage/HowItWorks.css";
 import { StepsModel } from "../../utils/models/StepsMode";
 import { useState } from "react";
 import { PiHandDepositFill } from "react-icons/pi";
+import { SiLinuxmint } from "react-icons/si";
+import { MdOutlineElectricalServices } from "react-icons/md";
+import { MdOutlineRedeem } from "react-icons/md";
+
+
+
 
 export function HowItWorks(){
     const steps = [
-      new  StepsModel("STEP 01", "Deposit", "BTC, ETH AND MORE AS A COLLATERAL", <PiHandDepositFill size={90}/>)
+      new  StepsModel("STEP 01", "Deposit", "BTC, ETH AND MORE AS A COLLATERAL", <PiHandDepositFill size={90}/>),
+      new StepsModel('STEP 02', 'Mint', 'MINT USDNEXUS BACKED UP BY YOUR COLLATERAL', <SiLinuxmint size={90}/>),new StepsModel('STEP 03', 'Explore', 'EXPLORE DEFI, DEXES USING USDNEXUS SUPERCHARGING YOUR EXPERIENCE', <MdOutlineElectricalServices size={90}/>),
+      new StepsModel('STEP 04', 'Redeem Collateral', 'BURN USDNEXUS TO REDEEM CRYPTO COLLATERAL DEPOSITED', <MdOutlineRedeem/>)
     ];
     const [index, updateIndex] = useState(0);
     const chainsDetails = [{title: "30+ Assets", details: "WETH,WBTC AND MORE"}, {title: "15+ Platforms", details: "UNISWAP,AAVE AND MORE"}, {title: "10+Chains", details: "ETH, POLYGON AND MORE"}];
+
+    function check(){
+        if(index === 0){
+            return 'step-one';
+        }else if(index === 1){
+            return 'step-two';
+        }else if (index === 2 ){
+            return 'step-three';
+        }else{
+            return 'step-four';
+        }
+    }
 
     function updateSteps(){
         if(index === steps.length -1){
@@ -21,7 +41,7 @@ export function HowItWorks(){
         <h1 id="how-title">
             How it Works
         </h1>
-        <div id="progress-container">
+        <div id="progress-container" className={`${check()}`}>
         <h4>
             {steps[index].stepNumber}
         </h4>
